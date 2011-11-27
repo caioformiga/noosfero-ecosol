ff715ee0239491a6320f1d95fb4d0cafa917d629
Braulio Bhavamitra <brauliobo@gmail.com>
2011-11-27 18:51:05 -0200

    Fix database on travis

--- ci/travis.rb
+++ ci/travis.rb
@@ -7,6 +7,7 @@ commands = [
   "cp config/database.yml.travis config/database.yml",
   "psql -c 'create database myapp_test;' -U postgres",
   "mkdir -p tmp/pids log",
+  "gem update --system 1.4.1",
 ]
 commands.each do |command|
   system "#{command} > /dev/null 2>&1"
